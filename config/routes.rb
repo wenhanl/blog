de678f62f61a9435522e3fdf79725ec2ab441f75
wenhanl <lvwenhan417@gmail.com>
2015-06-28 19:38:49 +0800

    clear unused model and production config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :admins
   root 'articles#index'
-  resources :users
 
   resources :articles
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625110043) do
+ActiveRecord::Schema.define(version: 20150628112616) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -50,14 +50,6 @@ ActiveRecord::Schema.define(version: 20150625110043) do
   create_table "tags", force: true do |t|
     t.string  "name"
     t.integer "blog_count"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.boolean  "active"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
