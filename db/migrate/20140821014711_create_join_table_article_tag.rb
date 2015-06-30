c3d5d4c079281f2dc22bd305c19fba7731ab26d6
wenhanl <lvwenhan417@gmail.com>
2015-06-30 18:12:00 +0800

    join table migration bug fixed

--- db/migrate/20140821014711_create_join_table_article_tag.rb
+++ db/migrate/20140821014711_create_join_table_article_tag.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableArticleTag < ActiveRecord::Migration
   def change
-	create_table :article_tag, id: false do |t|
+	create_table :articles_tags, id: false do |t|
       t.belongs_to :article, index: true
       t.belongs_to :tag, index: true
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,45 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628112616) do
-
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-
-  create_table "articles", force: true do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "img"
-  end
-
-  create_table "articles_tags", id: false, force: true do |t|
-    t.integer "article_id"
-    t.integer "tag_id"
-  end
-
-  add_index "articles_tags", ["article_id"], name: "index_articles_tags_on_article_id", using: :btree
-  add_index "articles_tags", ["tag_id"], name: "index_articles_tags_on_tag_id", using: :btree
-
-  create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "blog_count"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
