108eb08d861b0949d2cf1b4d044ac019440d0c53
wenhanl <lvwenhan417@gmail.com>
2015-07-02 15:10:11 -0700

    production config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -36,6 +36,8 @@ Rails.application.configure do
   config.assets.version = '1.0'
 
   config.assets.paths << Rails.root.join('/app/assets/fonts')
+
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
 
 
   # temp
