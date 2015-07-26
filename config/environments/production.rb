1d284330682b4180894dae55b0447422eaa26682
wenhanl <lvwenhan417@gmail.com>
2015-07-26 01:52:01 -0700

    added icon, changed precompile config, fixed css backgroud in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,12 +21,13 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = true
+  config.serve_static_assets = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
