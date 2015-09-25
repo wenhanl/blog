390d3835e2bcb27ebe5b51cdfd91f1c4061bc85a
wenhanl <lvwenhan417@gmail.com>
2015-09-24 22:29:35 -0700

    Use slick.js to make front page tab smoothly slides
    * Also update page icon
    * correct email address

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,6 @@ Rails.application.config.assets.precompile += %w( highlight/monokai_sublime.css 
 Rails.application.config.assets.precompile += %w( article_view.js);
 Rails.application.config.assets.precompile += %w( article_edit.js);
 Rails.application.config.assets.precompile += %w( toc.min.js);
-Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w( slick.css );
+Rails.application.config.assets.precompile += %w( slick.min.js );
+Rails.application.config.assets.precompile += %w( ckeditor/* );
